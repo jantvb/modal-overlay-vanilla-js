a41bcffefc186e2d19d446c058dab172bca52b04
jantvb <jantvb@gmail.com>
2024-06-27 20:43:40 -0400

    create modal-overlay

---
+++ README.md
@@ -0,0 +1,4 @@
+# Modal overlay - Vanilla JS template
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/josevicnte/pen/ZENZNMB](https://codepen.io/josevicnte/pen/ZENZNMB).
+
---
+++ dist/script.js
@@ -0,0 +1,24 @@
+/*
+* https://frontendeval.com/questions/modal-overlay
+*
+* Build a modal control and overlay
+*/
+const showOffer = document.getElementById('show-offer');
+const acceptOfferModal = document.getElementById('accept-offer-modal');
+const offerAccepted = document.getElementById('offer-accepted');
+
+function showModal() {
+  showOffer.classList.add('hide');
+  acceptOfferModal.classList.remove('hide');
+}
+
+function closeModal() {
+  acceptOfferModal.classList.add('hide');
+  showOffer.classList.remove('hide');
+}
+
+function offerAcceptedMsg() {
+  acceptOfferModal.classList.add('hide');
+  showOffer.classList.add('hide');
+  offerAccepted.classList.remove('hide');
+}
